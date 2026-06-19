876fbd7833a712a2c8c7f0610ffea0a3524bcd5c
Catherine Vuthi <vuthi.esther@gmail.com>
2026-06-19 10:36:45 +0300

    update config settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	reactStrictMode: false,
+	poweredByHeader: false,
+
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
