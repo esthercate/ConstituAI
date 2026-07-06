ba9ad11e89b31f41a19047a6106c4bffb69a9eb8
Catherine Vuthi <vuthi.esther@gmail.com>
2026-07-06 08:57:18 +0300

    Update home and layout pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import ThemeToggle from '@/components/layout/ThemeToggle';
 export default function Home() {
 	return (
-		<div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-			<h1>Hello World</h1>
+		<div className="flex flex-col flex-1 items-center justify-center">
+			<ThemeToggle />
+			<h1>Ask the Constitution</h1>
 		</div>
 	);
 }
