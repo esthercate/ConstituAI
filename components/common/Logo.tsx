6b24df0b2bce637a83f2c2df172eeade9125fc8d
Catherine Vuthi <vuthi.esther@gmail.com>
2026-07-07 08:51:55 +0300

    Add logo component

---
+++ components/common/Logo.tsx
@@ -0,0 +1,18 @@
+import { GoLaw } from 'react-icons/go';
+import Link from 'next/link';
+
+const Logo = () => {
+  return (
+    <div className='flex items-center justify-between'>
+      <Link href="/" className='flex items-center gap-2'>
+        <GoLaw size={40} />
+        <div>
+          <p className='text-xl font-bold'>Constitu<span className='text-primary'>AI</span></p>
+          <p className='text-sm text-muted-foreground'>Understand. Empower. Uphold.</p>
+        </div>
+      </Link>
+		</div>
+	);
+}
+
+export default Logo
