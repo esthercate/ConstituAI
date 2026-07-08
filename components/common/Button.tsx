458b2cb7c112d8b681bafb46a911cc7094217edd
Catherine Vuthi <vuthi.esther@gmail.com>
2026-07-08 08:49:39 +0300

    Add button ui

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({
+	children,
+	className,
+}: {
+	children: React.ReactNode;
+	className?: string;
+}) => {
+	return (
+		<button
+			className={`rounded-full border border-border bg-surface-muted py-1.5 px-4 text-foreground font-medium hover:text-primary transition-colors shadow-lg hover:shadow-xl cursor-pointer ${className}`}
+		>
+			{children}
+		</button>
+	);
+};
 
 export default Button
