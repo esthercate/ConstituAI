2348011ac966c1802220b151075ace5c3bc435cb
Catherine Vuthi <vuthi.esther@gmail.com>
2026-07-08 08:49:55 +0300

    Add logo responsiveness

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -3,14 +3,21 @@ import Link from 'next/link';
 
 const Logo = () => {
   return (
-    <div className='flex items-center justify-between'>
-      <Link href="/" className='flex items-center gap-2'>
-        <GoLaw size={40} />
-        <div>
-          <p className='text-xl font-bold'>Constitu<span className='text-primary'>AI</span></p>
-          <p className='text-sm text-muted-foreground'>Understand. Empower. Uphold.</p>
-        </div>
-      </Link>
+		<div className="flex items-center justify-between">
+			<Link
+				href="/"
+				className="flex items-center gap-2"
+			>
+				<GoLaw size={40} />
+				<div>
+					<p className="text-lg lg:text-xl font-bold">
+						Constitu<span className="text-primary">AI</span>
+					</p>
+					<p className="text-xs lg:text-sm text-muted-foreground">
+						Understand. Empower. Uphold.
+					</p>
+				</div>
+			</Link>
 		</div>
 	);
 }
