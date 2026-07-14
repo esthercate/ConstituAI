1ddcfe1583f335de13e3216027ec14987996d358
Catherine Vuthi <vuthi.esther@gmail.com>
2026-07-14 09:19:22 +0300

    Complete search bar

--- components/landing/SearchBar.tsx
+++ components/landing/SearchBar.tsx
@@ -2,21 +2,25 @@ import { FiArrowRight, FiSearch } from 'react-icons/fi';
 
 const SearchBar = () => {
 	return (
-		<div className="card">
-			<FiSearch className="text-primary text-3xl" />
-			<input
-				type="text"
-				placeholder="Type your constitutional question here..."
-				className="flex-1 bg-transparent text-foreground placeholder:text-muted-foreground text-base outline-none"
-			/>
+		<div className="container">
+			<div className="card flex justify-between">
+				<div className="flex gap-4 w-full items-center">
+					<FiSearch className="text-primary text-3xl" />
+					<input
+						type="text"
+						placeholder="Ask the constitution..."
+						className="flex w-full bg-transparent text-foreground placeholder:text-muted-foreground/60 text-base outline-none"
+					/>
+				</div>
 
-			<button
-				type="button"
-				className="flex items-center gap-2 rounded-xl bg-button px-8 py-4"
-			>
-				<FiArrowRight className="text-lg" />
-				Ask
-			</button>
+				<button
+					type="button"
+					className="flex items-center gap-2 text-white rounded-xl bg-button px-4 sm:px-8 py-3"
+				>
+					<FiArrowRight className="text-lg" />
+					<span className="hidden sm:inline">Ask</span>
+				</button>
+			</div>
 		</div>
 	);
 };
