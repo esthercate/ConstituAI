cfd190be8fa3b430d39b0ff963dcd3a018ff4be0
Catherine Vuthi <vuthi.esther@gmail.com>
2026-07-15 08:40:47 +0300

    Fix responsiveness

--- components/landing/SuggestedQuestions.tsx
+++ components/landing/SuggestedQuestions.tsx
@@ -1,45 +1,48 @@
-import React from 'react'
 import { GoLaw } from 'react-icons/go';
 import { FiUsers } from 'react-icons/fi';
 import { GiHandcuffs } from 'react-icons/gi';
 
+const suggestedQuestions = [
+	{
+		icon: GoLaw,
+		question: 'What are my rights to freedom of speech?',
+	},
+	{
+		icon: FiUsers,
+		question: 'What does the Constitution say about devolution?',
+	},
+	{
+		icon: GiHandcuffs,
+		question: 'Can I be arrested without a reason?',
+	},
+];
+
 const SuggestedQuestions = () => {
-  return (
+	return (
 		<div className="container">
-			<div className="flex flex-col gap-1">
+			<div className="mx-0 flex flex-col gap-3 lg:mx-56">
 				<span className="text-muted-foreground">Try asking</span>
-				<div className="flex flex-col md:flex-row gap-5 items-center">
-					<div className="card w-full flex gap-4 items-center">
-						<GoLaw
-							size={30}
-							className="text-primary"
-						/>
-						<span className="text-sm text-muted-foreground text-balance">
-							What are my rights to freedom of speech?
-						</span>
-					</div>
-					<div className="card w-full flex gap-4 items-center">
-						<FiUsers
-							size={30}
-							className="text-primary"
-						/>
-						<span className="text-sm text-muted-foreground text-balance">
-							What does the Constitution say about devolution?
-						</span>
-					</div>
-					<div className="card w-full flex gap-4 items-center">
-						<GiHandcuffs
-							size={30}
-							className="text-primary"
-						/>
-						<span className="text-sm text-muted-foreground text-balance">
-							Can I be arrested without a reason?
-						</span>
-					</div>
+
+				<div className="flex flex-col items-center gap-5 md:flex-row">
+					{suggestedQuestions.map(({ icon: Icon, question }) => (
+						<div
+							key={question}
+							className="card flex w-full items-center gap-4 p-3 lg:p-5"
+						>
+							<Icon
+								size={40}
+								className="text-primary"
+							/>
+
+							<span className="text-sm text-balance text-muted-foreground">
+								{question}
+							</span>
+						</div>
+					))}
 				</div>
 			</div>
 		</div>
 	);
-}
+};
 
-export default SuggestedQuestions
+export default SuggestedQuestions;
