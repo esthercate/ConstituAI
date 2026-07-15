af7c8a142ffead3b7eeb98af1cf2a97542d6d19c
Catherine Vuthi <vuthi.esther@gmail.com>
2026-07-15 08:41:19 +0300

    Update globals and page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from '@/components/layout/Navbar';
 import Hero from '@/components/landing/Hero';
 import SearchBar from '@/components/landing/SearchBar';
 import SuggestedQuestions from '@/components/landing/SuggestedQuestions';
-// import TrustedBadges from '@/components/landing/TrustedBadges';
+import TrustedBadges from '@/components/landing/TrustedBadges';
 export default function Home() {
 	return (
 		<div className="min-h-screen flex flex-col items-center justify-center">
@@ -12,7 +12,7 @@ export default function Home() {
 				<Hero />
 				<SearchBar />
 				<SuggestedQuestions />
-				{/* <TrustedBadges /> */}
+				<TrustedBadges />
 			</main>
 			<Footer />
 		</div>
