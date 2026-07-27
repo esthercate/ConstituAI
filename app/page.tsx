07c2d63a9ea2afbed51da81dcdf54496a5445219
Catherine Vuthi <vuthi.esther@gmail.com>
2026-07-27 10:05:04 +0300

    Add AIanswer  section component

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import Hero from '@/components/landing/Hero';
 import SearchBar from '@/components/landing/SearchBar';
 import SuggestedQuestions from '@/components/landing/SuggestedQuestions';
 import TrustedBadges from '@/components/landing/TrustedBadges';
+import QuestionCard from '@/components/conversation/QuestionCard';
+import AiAnswerSection from '@/components/conversation/AiAnswerSection';
+
 export default function Home() {
 	return (
 		<div className="min-h-screen flex flex-col items-center justify-center bg-[url(/bgimage.png)] dark:bg-[url('/darkbgimage.png')] bg-cover bg-center bg-no-repeat">
@@ -13,6 +16,8 @@ export default function Home() {
 				<SearchBar />
 				<SuggestedQuestions />
 				<TrustedBadges />
+				<QuestionCard />
+				<AiAnswerSection />
 			</main>
 			<Footer />
 		</div>
